73b9ccffb2baa30fc2f7cb4771078ef695a32cb8
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-05-23 01:43:48 +0200

    tasks: extract scripte from html

--- tasks/gulp/es6.js
+++ tasks/gulp/es6.js
@@ -8,10 +8,9 @@ var $ = require('gulp-load-plugins')();
 
 // Transpile all JS to ES6.
 gulp.task('ES6', () => {
-  var CSP_ALL = GLOBAL.config.CSP_ALL;
   return gulp.src(['src/**/*.{js,html}', '!src/bower_components/**/*'])
   .pipe($.if(GLOBAL.config.env === 'dev', $.sourcemaps.init()))
-  .pipe($.if('*.html' && CSP_ALL, $.crisper({scriptInHead: false}))) // Extract JS from .html files
+  .pipe($.if('*.html', $.crisper({scriptInHead: false}))) // Extract JS from .html files
   .pipe($.if('*.js', $.babel({
     presets: ['es2015']
   })))
