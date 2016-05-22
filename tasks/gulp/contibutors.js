75d3643e7d5e8c28f1edbe7de2818abab04f6abd
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-05-23 00:44:04 +0200

    tasks: promise them all! & read from file on 'dev'

--- tasks/gulp/contibutors.js
+++ tasks/gulp/contibutors.js
@@ -1,46 +1,184 @@
 'use strict';
 var gulp = require('gulp');
 var fs = require('fs');
-var github = require('./../github');
-var replace = require('gulp-replace');
 var settings = require('./../settings')();
+var github = require('./../github')({repo: settings.repo});
+var replace = require('gulp-replace');
 
-var injectTask = (contributors, cb) => {
-  fs.writeFileSync('src/contributors.json', contributors);
-  return gulp.src('.tmp/**/*')
-    .pipe(replace(/'@inject:contributors'/g, contributors))
-    .pipe(gulp.dest('.tmp'))
-    .pipe(gulp.dest(GLOBAL.config.dest))
-    .on('end', () => {
-      cb();
-    });
+if (settings.files &&
+    settings.files.contributors &&
+    settings.files.contributors.write) {
+  GLOBAL.CONTRIBUTORS_CONFIG = settings.files.contributors;
+}
+
+/**
+ * injects the contributors into your app using the choosen 'inject-tag'
+ * Example: '@inject:contributors'
+ * This can be configured in the [.ioconfig](/.ioconfig) file
+ * @param {string} contributors an string array
+ * @return {Promise} returns an promise
+ * @alias tasks/contributors
+ */
+var injectTask = contributors => {
+  return new Promise(function(resolve, reject) {
+    try {
+      if (!JSON.parse(contributors).length) {
+        contributors = [];
+      }
+      gulp.src(`${GLOBAL.config.dest}/**/*`)
+        .pipe(replace(/'@inject:contributors'/g, contributors))
+        .pipe(gulp.dest(GLOBAL.config.dest))
+        .on('end', () => {
+          resolve(contributors);
+        });
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
 
-gulp.task('contributors:get', cb => {
-  if (settings.github) {
-    github.contributors(settings.repo).then(contributors => {
-      if (JSON.parse(contributors).message && JSON.parse(contributors).message.includes('API rate limit exceeded')) {
-        console.warn('contributors aren\'t updated\nAPI rate limit exceeded\nFalling back to contributors.json file');
-        injectTask(fs.readFileSync('src/contributors.json', 'utf-8'), () => {
-          cb();
-        });
-      } else {
-        // fs.writeFileSync('src/contributors.json', contributors);
-        if (!contributors.length) {
-          contributors = [""];
-        }
+/**
+ * Reads the contributors file
+ *
+ * @param {object} config {name: String, dest: String, write: Boolean, inject: RegExp}
+ * @return {Promise} return an promise with contributors or an error
+ */
+var readTask = config => {
+  return new Promise(function(resolve, reject) {
+    try {
+      var contributors = fs.readFileSync(`${config.dest}/${config.name}.json`);
+      resolve(contributors);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-        injectTask(contributors, () => {
+/**
+ * Writes the contributors file
+ *
+ * @param {array} contributors an array containing the contributors
+ * @param {object} config {name: String, dest: String, write: Boolean, inject: RegExp}
+ * @return {Promise} returns an promise
+ */
+var writeTask = (contributors, config) => {
+  return new Promise(function(resolve, reject) {
+    try {
+      if (settings.files &&
+        settings.files.contributors &&
+        settings.files.contributors.write) {
+        fs.writeFileSync(`${config.dest}/${config.name}.json`, contributors);
+      }
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+/**
+ * Try's to get the contributors from github
+ *
+ * @param {object} config {name: String, dest: String, write: Boolean, inject: RegExp}
+ * @return {Promise} returns an promise
+ */
+var getGithubRepoContributors = config => {
+  return new Promise(function(resolve, reject) {
+    github.contributors.then(contributors => {
+      injectTask(contributors)
+      .then(writeTask(contributors, config)
+        .then(() => {
+          resolve();
+        }));
+    })
+    .catch(err => {
+      reject(err);
+    });
+  });
+};
+
+var githubContributorsTask = config => {
+  return new Promise(function(resolve, reject) {
+    getGithubRepoContributors(config)
+    .then(() => {
+      resolve();
+    })
+    .catch(() => {
+      // something wen't wrong getting the contributors, falling back to .json file (if it exist already)
+      try {
+        readTask(config).then(contributors => {
+          injectTask(contributors).then(() => {
+            resolve();
+          });
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  });
+};
+
+/**
+ * Returns an Array of the contributors, control wether or not to write a file, in [.ioconfig](/.ioconfig) @line:10
+ * @mixin settings
+ * @mixin github
+ * @mixin gulp-tasks
+ */
+gulp.task('contributors:get', cb => {
+  var config = {
+    dest: GLOBAL.CONTRIBUTORS_CONFIG.dest,
+    name: GLOBAL.CONTRIBUTORS_CONFIG.name
+  };
+  var url = '[.ioconfig](.ioconfig)';
+  // we don't wanne spam the github api for every change we do
+  if (GLOBAL.config.env === 'dev') {
+    console.log(`getting contributors file ${config.name}.json
+        from ${config.dest}
+        defined in ${url} file it's 'files' (object)`);
+    // check if there is an contributors.json file
+    readTask(config).then(contributors => {
+      // inject the contributors
+      injectTask(contributors).then(() => {
+        cb();
+      });
+    }).catch(() => {
+      console.warn(`no contributors.json file found
+      trying to get data from github`);
+      // when github is enabled in settings
+      if (settings.github) {
+      // try getting the contributors from github
+        console.log(`getting contributors from Github (${url} file)`);
+        githubContributorsTask(config).then(() => {
           cb();
         });
       }
     });
+  } else if (settings.github) {
+    // get the contributors from github
+    console.log(`getting contributors from Github (${url} file)`);
+    githubContributorsTask(config).then(() => {
+      cb();
+    });
   } else if (settings.contributors) {
-    var _contributors = [];
-    if (settings.contributors.length) {
-      _contributors = settings.contributors;
-    }
-    injectTask(_contributors, () => {
+    console.log(`getting contributors from settings (${url} file)`);
+    // get the contributors from setting
+    injectTask(settings.contributors, () => {
+      cb();
+    });
+  } else {
+    console.log(`getting contributors file ${config.name}.json
+        from ${config.dest}
+        defined in ${url} file it's 'files' (object)`);
+    readTask(config).then(contributors => {
+      // inject the contributors
+      injectTask(contributors).then(() => {
+        cb();
+      });
+    }).catch(() => {
+      console.log(`no contributors file found:
+        file: ${config.name}.json
+        path: ${config.dest}
+        defined in ${url} file it's 'files' (object)`);
       cb();
     });
   }
