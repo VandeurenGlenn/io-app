7fcf796d8d8655a64fa969b09c7c5c3ae2447768
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-05-23 01:04:39 +0200

    tasks: add CSP_ALL

--- tasks/gulp/es6.js
+++ tasks/gulp/es6.js
@@ -8,20 +8,20 @@ var $ = require('gulp-load-plugins')();
 
 // Transpile all JS to ES6.
 gulp.task('ES6', () => {
- return gulp.src(['src/**/*.{js,html}', '!src/bower_components/**/*'])
-  .pipe($.sourcemaps.init())
-  .pipe($.if('*.html', $.crisper({scriptInHead: false}))) // Extract JS from .html files
+  var CSP_ALL = GLOBAL.config.CSP_ALL;
+  return gulp.src(['src/**/*.{js,html}', '!src/bower_components/**/*'])
+  .pipe($.if(GLOBAL.config.env === 'dev', $.sourcemaps.init()))
+  .pipe($.if('*.html' && CSP_ALL, $.crisper({scriptInHead: false}))) // Extract JS from .html files
   .pipe($.if('*.js', $.babel({
     presets: ['es2015']
   })))
-  .pipe($.sourcemaps.write('.'))
-  .pipe(gulp.dest('.tmp/'))
+  .pipe($.if(GLOBAL.config.env === 'dev', $.sourcemaps.write('.')))
   .pipe(gulp.dest(GLOBAL.config.dest))
   .pipe($.size({title: 'ES6'}));
 });
 
 gulp.task('ES6:todo', () => {
- return gulp.src(['todo/**/**.{js,html}'])
+  return gulp.src(['todo/**/**.{js,html}'])
   .pipe($.sourcemaps.init())
   .pipe($.if('*.html', $.crisper({scriptInHead: false}))) // Extract JS from .html files
   .pipe($.if('*.js', $.babel({
