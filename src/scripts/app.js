ba51401fbdeff71ee0ff4783a57d7e4f4b753929
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-05-23 00:36:22 +0200

    inject-appname

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,16 +1,6 @@
 (function() {
   'use strict';
-  var app = document.querySelector('#app');
-  app.appName = 'io-app';
-  app.github = {
-    account: 'vandeurenglenn',
-    url: 'https://github.com/vandeurenglenn',
-    projects: [],
-    contributors: [{
-      name: 'glenn',
-      familyname: 'vandeuren',
-      avatar: 'avatar',
-      link: 'link'
-    }]
-  };
+  console.log('scripts loaded');
+  var ioApp = document.querySelector('io-app');
+  ioApp.appName = '@inject:appname';
 })();
