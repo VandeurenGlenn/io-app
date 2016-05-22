655298df4dbdce577988a9d2b598a7c6d64653b8
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-05-23 01:45:39 +0200

    Tasks: inject appname

--- tasks/gulp/inject.js
+++ tasks/gulp/inject.js
@@ -1,4 +1,15 @@
 'use strict';
 var gulp = require('gulp');
+var replace = require('gulp-replace');
+var settings = require('./../settings')();
 
-gulp.task('inject', gulp.series('contributors'));
+gulp.task('appname', cb => {
+  gulp.src(`${GLOBAL.config.dest}/**/*`)
+    .pipe(replace(settings.injectTags.appname, settings.appname))
+    .pipe(gulp.dest(GLOBAL.config.dest))
+    .on('end', () => {
+      cb();
+    });
+});
+
+gulp.task('inject', gulp.series('appname', 'contributors'));
